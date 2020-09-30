53eec3318c2958583921e082df83641a17b2c4a2
Axcell Collins-Mena <axcell.collins-mena@daviestech.org>
2020-09-30 21:41:28 +0000

    I explored how the input and output commands along with using charcaters

---
+++ question5.py
@@ -0,0 +1,4 @@
+first = input("Enter a word: ")
+
+second = input("Enter a word: ")
+print(first+" "+second)
---
+++ question6.py
@@ -0,0 +1,3 @@
+name1 = input("Please input your first name: ")
+name2 = input("Please input your last name: ")
+print(name2+", "+name1 )
